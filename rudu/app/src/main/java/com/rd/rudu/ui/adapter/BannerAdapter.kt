ce87897a68f8ecaee0e1a53e79041956921f80f1
chin <chin@chinamcloud.com>
2020-08-28 19:25:31 +0800

    首页 我的图标

--- rudu/app/src/main/java/com/rd/rudu/ui/adapter/BannerAdapter.kt
+++ rudu/app/src/main/java/com/rd/rudu/ui/adapter/BannerAdapter.kt
@@ -1,12 +1,14 @@
 package com.rd.rudu.ui.adapter
 
 import android.content.Context
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.alibaba.android.vlayout.LayoutHelper
 import com.alibaba.android.vlayout.layout.DefaultLayoutHelper
 import com.google.android.app.adapter.AppVLayoutAdapter
 import com.google.android.app.adapter.BaseViewHolder
+import com.google.android.app.widget.LoopPager
 import com.rd.rudu.R
 import com.rd.rudu.databinding.AdapterJoinBannerBinding
 
@@ -17,14 +19,43 @@ class BannerAdapter(context: Context, layoutHelper: LayoutHelper= DefaultLayoutH
     {
        return BannerViewHolder(R.layout.adapter_join_banner,context)
     }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        super.onBindViewHolder(holder, position)
+        if(holder is BannerViewHolder)
+        {
+            holder.setData()
+        }
+    }
 }
 
 class BannerViewHolder(layoutId: Int, context: Context?) :
-    BaseViewHolder<AdapterJoinBannerBinding>(layoutId, context)
-{
+    BaseViewHolder<AdapterJoinBannerBinding>(layoutId, context), LoopPager.OnLooperPagerHandle {
     init {
+        contentViewBinding.banner.onLooperPagerHandle=this
         contentViewBinding.banner.setOnItemClickListener { banner, position, tag ->
 
         }
+    }
+
+    fun setData()
+    {
+    }
+
+    override fun createImageView(context: Context?, position: LoopPager.ImageItem?): View
+    {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoopPageSelected(index: Int) {
+    }
+
+    override fun displayImage(context: Context?, path: LoopPager.ImageItem?, view: View?)
+    {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoopPageSelectedReallyReallyIndex(index: Int)
+    {
     }
 }
