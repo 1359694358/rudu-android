d6ccbe6979863cd8475de197c3ee781627e279c1
dy <1359694358@qq.com>
2020-09-05 17:29:07 +0800

    加盟

--- rudu/app/src/main/java/com/rd/rudu/ui/activity/RuduIntroActivity.kt
+++ rudu/app/src/main/java/com/rd/rudu/ui/activity/RuduIntroActivity.kt
@@ -1,5 +1,7 @@
 package com.rd.rudu.ui.activity
 
+import android.os.Bundle
+import com.google.android.app.utils.ViewUtils
 import com.google.android.app.widget.BaseActivity
 import com.rd.rudu.R
 import com.rd.rudu.databinding.ActivityRuduintroBinding
@@ -11,5 +13,13 @@ class RuduIntroActivity : BaseActivity<ActivityRuduintroBinding>() {
     override fun getLayoutResId(): Int
     {
         return R.layout.activity_ruduintro
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTitle("誉都简介")
+        var color=0xFF222222.toInt()
+        toolbarBinding?.titleText?.setTextColor(color)
+        toolbarBinding?.backBtn?.setImageDrawable(ViewUtils.setDrawableColor(this,color,R.mipmap.icon_back_b))
     }
 }
