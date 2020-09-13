6fef379c5025c300fdb464bf6c6271cec87789d6
dy <1359694358@qq.com>
2020-09-13 13:39:56 +0800

    显示控制

---
+++ rudu/app/src/main/java/com/rd/rudu/net/AppModuleConfig.kt
@@ -0,0 +1,9 @@
+package com.rd.rudu.net
+object AppModuleConfig
+{
+    val appShowModules= mutableListOf<String>()
+    enum class Modules
+    {
+        招商加盟,申请加盟,招商联系我们,城市合伙人加盟,合伙人报名,合伙人联系及合作,品牌招商入驻,营销效果,平台优势,品牌联系及合作,誉都简介,关于誉都,精英团队,公司优势,主打产品,简介联系我们,展会信息,新品发布,展品专区,明星单品,合作品牌,展会联系我们,爆款榜单,好货推荐,新品尝鲜
+    }
+}
